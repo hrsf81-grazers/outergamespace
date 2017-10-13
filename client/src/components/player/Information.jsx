134410422d13a316fea16b67d6eb7309d4ee8824
Jonathan Ling <electric.pen@gmail.com>
2017-10-13 15:34:00 -0700

    Implement basic styling for information text cards

--- client/src/components/player/Information.jsx
+++ client/src/components/player/Information.jsx
@@ -12,10 +12,10 @@ class Information extends React.Component {
 
   render() {
     return(
-      <div className={`container-fluid gameBackground ${this.state.visibility}`}>
-        <div className="card">
-          <div className="card-title">
-            {this.state.text}
+      <div className={`container-fluid`}>
+        <div className="gameBackground row align-items-center justify-content-md-center">
+          <div className={`card col-lg-4 col-md-5 col-sm-9 col-xs-12 informationCard animated slideInLeft ${this.state.visibility}`}>
+            <div className="card-block informationText">{this.state.text}</div>
           </div>
         </div>
       </div>
