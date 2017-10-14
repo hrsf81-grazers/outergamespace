5f14340e46fd605c88b3e93a3e9b925eebdd8d7f
Jonathan Ling <electric.pen@gmail.com>
2017-10-13 21:27:22 -0700

    Implement user badges and dynamic rendering logic to display them on the leaderboard

--- client/src/components/player/LeaderboardEntry.jsx
+++ client/src/components/player/LeaderboardEntry.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Badge from './Badge';
+
 
 const LeaderboardEntry = props => (
   <li className="list-group-item bg-transparent leaderboardText">
     <span>{`${props.index + 1}. ${props.user.name}   ${props.user.total_points}pts`}</span>
-    <span className="badge-dark">{props.user.games_played}</span>
+    {props.user.badge.split(',').map((badge, index) => {
+      return <Badge badge={badge} index={index}/>
+    })}
+    <span className="badge-dark float-right">{props.user.games_played}</span>
   </li>
 );
 
--- db/schema.sql
+++ db/schema.sql
@@ -22,4 +22,20 @@ CREATE TABLE games(
 );
 
 INSERT INTO users (name, hash, total_points, games_played, badge)
-VALUES ('Backbone', '$2a$10$WZDCgP6R1OiAIiHYUtXAe.UPvJp0ez.DqSv58E3BJK2drc8yVmj2a', 10000, 100, 'hacker');
+VALUES ('Backbone', '$2a$10$WZDCgP6R1OiAIiHYUtXAe.UPvJp0ez.DqSv58E3BJK2drc8yVmj2a', 10000, 100, 'onFire,hacker');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Leo', '$2a$10$/uog2cLlSxi/dXDJPQhBZ.xVGxLE03Y8.o4Jb/SahLaJS0A/MLahC', 4080, 76, 'onFire,champ');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Rebecca', '$2a$10$z2Lr2WlpmXxHOBz/Hk9XI.DBHI8o/7Frv02rZiAf7kmWkor4Itc42', 3750, 72, 'champ');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Lynn', '$2a$10$1VLtqj9qsgl3MYM5ZhZCFuQq24iWoENmgTVRF7Z0Gpfb.gD080.MW', 3020, 67, 'champ');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('David', '$2a$10$zw6RbM4M/MpD188Zr3U20.ylSBYRuizdG/e1cCdUF7jLa5gWlfIme', 2980, 61, 'grazer');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Lam', '$2a$10$NNULfpWpOVrbGuSyFTBjmuHdilWmzuBIntpOjRhSo8RQYgfmcQEmO', 2830, 55, 'newb');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Jon', '$2a$10$OcWWvxq29ZuSEL3rz/aGAe3rZyUUd6.7X.omMYBxAt3pc6qAy7DPS', 2470, 77, 'newb,grazer');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Christine', '$2a$10$f16wZvuQVuI0p00Wg5djFecMzPrZv3sS2lsvGwEfYTZFeDN9K.2dm', 2010, 42, 'newb,grazer');
+INSERT INTO users (name, hash, total_points, games_played, badge)
+VALUES ('Austin', '$2a$10$9E6DqHNNyt05J7T0aBvWUuKtipmUsC50GlVpxF/3jYTZvh4bB4Jme', 10, 1, 'turd,grazer');
