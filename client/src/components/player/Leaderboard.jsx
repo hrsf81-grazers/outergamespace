5818f481ae8692eee14233b69c6d432f66fcbf73
Austin Sloane <sloane.ats@gmail.com>
2017-10-14 17:05:10 -0700

    limit leaderboard to 14 entries

--- client/src/components/player/Leaderboard.jsx
+++ client/src/components/player/Leaderboard.jsx
@@ -5,6 +5,7 @@ const Leaderboard = props => (
   <div className={`col-sm-3 leaderboard mr-3 ${props.leaderboardRender}`}>
     <ol className="list-group">
       {props.users.sort((a, b) => b.total_points - a.total_points)
+        .slice(0, 14)
         .map((user, index) => <LeaderboardEntry key={user.id} user={user} index={index}/>)}
     </ol>
   </div>
