af8811dbd83187a19771fd8d060786a92b34e47c
Jonathan Ling <electric.pen@gmail.com>
2017-10-15 11:43:25 -0700

    Implement bootstrap styling to game info dropdowns

--- client/src/components/player/DropDown.jsx
+++ client/src/components/player/DropDown.jsx
@@ -12,25 +12,46 @@ class DropDown extends React.Component {
   toggle() {
     this.setState({ listVisible: !this.state.listVisible });
   }
-
   render() {
     return (
-      <div className={'dropdown-container'}>
-        <div className={'dropdown-display'} onClick={this.toggle}>
-          <p>{this.props.title}'s Game</p>
-        </div>
-        <div
-          className={'dropdown-list' + (this.state.listVisible ? "" : " hide")}
-          onClick={this.props.click}
-        >
-          <p>Room Code: {this.props.game.room_id}</p>
-          <p># Questions: {this.props.game.num_questions}</p>
-          <p>Max Players: {this.props.game.max_players}</p>
-          <p># Players: {this.state.numPlayers}</p>
+      <div>
+        <p>
+          <button className="btn btn-outline-light ml-1 mt-3" type="button" data-toggle="collapse" data-target="#gameInformation" aria-expanded="false" aria-controls="gameInformation">
+            {this.props.title}'s Game'
+          </button>
+        </p>
+      <div className="collapse ml-1 mb-3" id="gameInformation">
+        <div className="card list-group list-group-flush">
+         <p className="list-group-item"># Questions: {this.props.game.num_questions}</p>
+         <p className="list-group-item">Max Players: {this.props.game.max_players}</p>
+         <p className="list-group-item"># Players: {this.state.numPlayers}</p>
+         <p className="list-group-item">Room Code: {this.props.game.room_id}</p>
+         <button onClick={this.props.click} className="list-group-item joinButton">Join Game!</button>
         </div>
       </div>
-    );
+      </div>
+    )
   }
+
 }
 
 export default DropDown;
+
+// render() {
+//   return (
+//     <div className={'dropdown-container'}>
+//       <div className={'dropdown-display'} onClick={this.toggle}>
+//         <p>{this.props.title}'s Game</p>
+//       </div>
+//       <div
+//         className={'dropdown-list' + (this.state.listVisible ? "" : " hide")}
+//         onClick={this.props.click}
+//       >
+//         <p>Room Code: {this.props.game.room_id}</p>
+//         <p># Questions: {this.props.game.num_questions}</p>
+//         <p>Max Players: {this.props.game.max_players}</p>
+//         <p># Players: {this.state.numPlayers}</p>
+//       </div>
+//     </div>
+//   );
+// }
